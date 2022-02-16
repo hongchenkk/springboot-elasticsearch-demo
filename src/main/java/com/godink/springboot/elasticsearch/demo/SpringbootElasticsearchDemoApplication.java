6cd3d8ab2b158182b4d7caa23fd28f65a7fc4d78
hong.chen <hong.chen@audaque.com>
2022-02-16 10:16:06 +0800

    项目搭建

---
+++ src/main/java/com/godink/springboot/elasticsearch/demo/SpringbootElasticsearchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.godink.springboot.elasticsearch.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootElasticsearchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootElasticsearchDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godink/springboot/elasticsearch/demo/SpringbootElasticsearchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godink.springboot.elasticsearch.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootElasticsearchDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
